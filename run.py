842ee3941435471e6d252f96ed480fcbac492a0b
lemocla <lemocla@icloud.com>
2021-05-26 13:37:20 +0000

    Add template inheritance for nav in base.html & careers.html

--- run.py
+++ run.py
@@ -22,15 +22,15 @@ def contact():
     return render_template("contact.html")
 
 
-if __name__ == "__main__" :
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
+if __name__ == "__main__":
     app.run(
-        host = os.environ.get("IP", "0.0.0.0"),
-        port = int(os.environ.get("PORT", "5000")), # "5000" common port used by flast
-        debug = True
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
     )
-
-
-
-
-
-
+    # "5000" common port used by flask.
